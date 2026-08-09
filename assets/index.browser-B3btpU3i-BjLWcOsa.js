6aa1961f52d21220cfa0bffee6a04f5e93412562
anderslemke <anderslemke@users.noreply.github.com>
2026-08-09 18:42:02 +0000

    deploy: 93a4fe6316e7c4cdd0f94aa5f14b57f52a651b08

--- assets/index.browser-B3btpU3i-BjLWcOsa.js
+++
@@ -1,1 +0,0 @@
-var n="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict",r=(a=21)=>{let e="",t=crypto.getRandomValues(new Uint8Array(a|=0));for(;a--;)e+=n[t[a]&63];return e};export{r as nanoid};
